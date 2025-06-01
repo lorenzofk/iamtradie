918cfa6b97a95a228fb4a8acb03c1818905ce26e
Lorenzo K <43301904-lorenzokniss@users.noreply.replit.com>
2025-06-01 01:33:11 +0000

    add: twillio service implementation

--- config/services.php
+++ config/services.php
@@ -35,4 +35,11 @@ return [
         ],
     ],
 
+    'twilio' => [
+        'sid' => env('TWILIO_SID'),
+        'token' => env('TWILIO_AUTH_TOKEN'),
+        'from' => env('TWILIO_FROM_NUMBER'),
+        'webhook_url' => env('TWILIO_WEBHOOK_URL'),
+    ],
+
 ];
