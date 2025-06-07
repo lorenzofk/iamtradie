a5d42f0c2e66b65aeb2086834ff5fa48afcdafc3
Lorenzo Kniss <lorenzo.kniss@gmail.com>
2025-06-07 18:43:38 +1000

    Fix: check if telescope class exists

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,11 +11,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local')) {
+        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(\App\Providers\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
         }
     }
+
+
 
     /**
      * Bootstrap any application services.
