035b844b42367d0870a8d1d60bbe26655c327a97
Lorenzo Kniss <lorenzo.kniss@gmail.com>
2025-06-12 18:25:38 +1000

    Fix: route name

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\TwilioController;
 
 Route::group(['prefix' => 'twilio', 'as' => 'twilio.'], function () {
     Route::post('/text', [TwilioController::class, 'incomingText'])->name('incoming-text');
-    Route::post('/call', [TwilioController::class, 'incomingCall'])->name('incoming-call');
+    Route::post('/voice', [TwilioController::class, 'incomingCall'])->name('incoming-call');
     Route::post('/no-answer', [TwilioController::class, 'missedCall'])->name('missed-call');
     Route::post('/transcription', [TwilioController::class, 'transcription'])->name('transcription');
     Route::post('/after-record', [TwilioController::class, 'afterRecord'])->name('after-record');
